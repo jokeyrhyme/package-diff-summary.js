e7e781c22e25f3f89137325bc9a19900794101bc
Ron Waldon <jokeyrhyme@gmail.com>
2017-01-10 02:16:27 +1100

    update-notifier and update-nodejs-notifier

--- bin/index.js
+++ bin/index.js
@@ -26,3 +26,9 @@ if (!cli.input[0]) {
   console.error(os.EOL + 'Error: no commit specified')
   cli.showHelp(1) // exits with process.exitCode = 1
 }
+
+const updateNotifier = require('update-notifier')
+updateNotifier({ pkg: cli.pkg }).notify()
+
+const { updateNodejsNotifier } = require('update-nodejs-notifier')
+updateNodejsNotifier()
