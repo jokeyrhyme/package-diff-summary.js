32e0e861d8cc5d32802f7a43ac8b84401c19da02
Ron Waldon <jokeyrhyme@gmail.com>
2017-01-10 04:52:49 +1100

    add NPM hyperlink; fix rollback/update bug

--- __tests__/diff.js
+++ __tests__/diff.js
@@ -9,14 +9,16 @@ test('deltaToMarkdown()', () => {
       { name: 'added', version: '1.2.3', operation: 'new' },
       { name: 'dropped', version: '', operation: 'delete' },
       { name: 'rolledback', version: '1.2.3', operation: 'edit' },
-      { name: 'updated', version: '2.3.4', operation: 'edit' }
+      { name: 'updated', version: '2.3.4', operation: 'edit' },
+      { name: 'execa', version: '2.3.4', operation: 'edit' },
     ]
   }
   const oldPkg = {
     dependencies: {
       dropped: '1.2.3',
       rolledback: '2.3.4',
-      updated: '1.2.3'
+      updated: '1.2.3',
+      execa: '1.2.3'
     }
   }
   return diff.deltaToMarkdown(delta, oldPkg)
