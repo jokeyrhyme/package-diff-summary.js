0af5f76b5b0796d4d11b51e631f9c01f89ca392d
Ron Waldon <jokeyrhyme@gmail.com>
2017-01-12 17:04:29 +1100

    new `changelogUrlAt(projectUrl)` helper

--- __tests__/github.js
+++ __tests__/github.js
@@ -3,17 +3,25 @@
 
 const github = require('../lib/github.js')
 
+test('changelogUrlAt("https://github.com/sindresorhus/got")', () => {
+  const projectUrl = 'https://github.com/sindresorhus/got'
+  return github.changelogUrlAt(projectUrl)
+    .then((result) => expect(result).toBe(''))
+})
+
+test('changelogUrlAt("https://github.com/tj/node-github-url-from-git")', () => {
+  const projectUrl = 'https://github.com/tj/node-github-url-from-git'
+  return github.changelogUrlAt(projectUrl)
+    .then((result) => expect(result).toMatchSnapshot())
+})
+
 test('projectUrlFor("does-not-exist")', () => {
   const name = 'does-not-exist-' + (Math.random() * 1e6)
   return github.projectUrlFor(name)
-    .then((result) => {
-      expect(result).not.toBeDefined()
-    })
+    .then((result) => expect(result).not.toBeDefined())
 })
 
 test('projectUrlFor("execa")', () => {
   return github.projectUrlFor('execa')
-    .then((result) => {
-      expect(result).toMatchSnapshot()
-    })
+    .then((result) => expect(result).toMatchSnapshot())
 })
