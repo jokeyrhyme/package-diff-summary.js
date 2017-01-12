a6230b89f35f4667dbe5a1222240e04bb3707839
Ron Waldon <jokeyrhyme@gmail.com>
2017-01-12 16:53:19 +1100

    extract `isUrlHealthy(testUrl)` for later use

---
+++ __tests__/http.js
@@ -0,0 +1,16 @@
+/* @flow */
+'use strict'
+
+const http = require('../lib/http.js')
+
+test('isUrlHealthy("https://www.npmjs.com/package/does-not-exist")', () => {
+  const name = 'does-not-exist-' + (Math.random() * 1e6)
+  const pkgUrl = 'https://www.npmjs.com/package/' + name
+  return http.isUrlHealthy(pkgUrl)
+    .then((result) => expect(result).toBe(false))
+})
+
+test('isUrlHealthy("http://google.com")', () => {
+  return http.isUrlHealthy('http://google.com')
+    .then((result) => expect(result).toBe(true))
+})
