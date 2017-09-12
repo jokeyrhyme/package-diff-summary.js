883528429abf55b3d2637302c27f53bea4fc48d8
Ron Waldon <jokeyrhyme@gmail.com>
2017-09-12 21:56:29 +1000

    run @jokeyrhyme/node-init

--- bin/index.js
+++ bin/index.js
@@ -3,39 +3,42 @@
 /* @flow */
 /* eslint-disable no-console */
 
-'use strict'
+'use strict';
 
-const os = require('os')
+const os = require('os');
 
-const meow = require('meow')
+const meow = require('meow');
 
-const cli = meow(`
+const cli = meow(
+  `
   Usage
     $ package-diff-summary <revision>
 
   Options
     -h, --help
     -v, --version
-`, {
-  alias: {
-    h: 'help',
-    v: 'version'
+`,
+  {
+    alias: {
+      h: 'help',
+      v: 'version',
+    },
   }
-})
+);
 
 if (!cli.input[0]) {
-  console.error(os.EOL + 'Error: no revision specified')
-  cli.showHelp(1) // exits with process.exitCode = 1
+  console.error(os.EOL + 'Error: no revision specified');
+  cli.showHelp(1); // exits with process.exitCode = 1
 }
 
-const updateNotifier = require('update-notifier')
-updateNotifier({ pkg: cli.pkg }).notify()
+const updateNotifier = require('update-notifier');
+updateNotifier({ pkg: cli.pkg }).notify();
 
-const { updateNodejsNotifier } = require('update-nodejs-notifier')
-updateNodejsNotifier()
+const { updateNodejsNotifier } = require('update-nodejs-notifier');
+updateNodejsNotifier();
 
-const { enginesNotify } = require('package-engines-notifier')
+const { enginesNotify } = require('package-engines-notifier');
 if (!enginesNotify({ pkg: cli.pkg })) {
-  const main = require('../index.js').main
-  main(cli.input, cli.flags)
+  const main = require('../index.js').main;
+  main(cli.input);
 }
--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 /* @flow */
-'use strict'
+'use strict';
 
-const readPkgUp = require('read-pkg-up')
+const readPkgUp = require('read-pkg-up');
 
-const git = require('./lib/git.js')
-const diff = require('./lib/diff.js')
+const git = require('./lib/git.js');
+const diff = require('./lib/diff.js');
 
-function main (
-  input /* : string[] */
-) {
-  let oldPkg
+function main(input /* : string[] */) {
+  let oldPkg;
 
   if (!process.env.GITHUB_OAUTH_TOKEN) {
     /* eslint-disable no-console */ // show tip for better user experience
     console.log(`
     TIP: set the GITHUB_OAUTH_TOKEN environment variable to improve accuracy
-`)
+`);
     /* eslint-enable no-console */
   }
 
   return Promise.all([
     readPkgUp({ cwd: process.cwd() }),
-    git.gitShow(input[0], 'package.json')
-      .then((oldPkgString) => JSON.parse(oldPkgString))
-      .then((result) => { oldPkg = result })
+    git
+      .gitShow(input[0], 'package.json')
+      .then(oldPkgString => JSON.parse(oldPkgString))
+      .then(result => {
+        oldPkg = result;
+      }),
   ])
-    .then(([ { pkg } ]) => diff.diffPackages(oldPkg, pkg))
-    .then((delta) => diff.deltaToMarkdown(delta, oldPkg))
-    .then((text) => {
+    .then(([{ pkg }]) => diff.diffPackages(oldPkg, pkg))
+    .then(delta => diff.deltaToMarkdown(delta, oldPkg))
+    .then(text => {
       /* eslint-disable no-console */ // CLI tool, relax!
-      console.log(text)
+      console.log(text);
       /* eslint-enable no-console */
-    })
+    });
 }
 
 module.exports = {
-  main
-}
+  main,
+};
