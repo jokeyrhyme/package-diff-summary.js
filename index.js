5be0dace88e403439fdb94a343222ffe93ee51e8
Ron Waldon <jokeyrhyme@gmail.com>
2017-09-12 22:01:48 +1000

    MAJOR: require Node.js 8.x or newer

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const readPkgUp = require('read-pkg-up');
 const git = require('./lib/git.js');
 const diff = require('./lib/diff.js');
 
-function main(input /* : string[] */) {
-  let oldPkg;
-
+async function main(input /* : string[] */) {
   if (!process.env.GITHUB_OAUTH_TOKEN) {
     /* eslint-disable no-console */ // show tip for better user experience
     console.log(`
@@ -17,22 +15,13 @@ function main(input /* : string[] */) {
     /* eslint-enable no-console */
   }
 
-  return Promise.all([
-    readPkgUp({ cwd: process.cwd() }),
-    git
-      .gitShow(input[0], 'package.json')
-      .then(oldPkgString => JSON.parse(oldPkgString))
-      .then(result => {
-        oldPkg = result;
-      }),
-  ])
-    .then(([{ pkg }]) => diff.diffPackages(oldPkg, pkg))
-    .then(delta => diff.deltaToMarkdown(delta, oldPkg))
-    .then(text => {
-      /* eslint-disable no-console */ // CLI tool, relax!
-      console.log(text);
-      /* eslint-enable no-console */
-    });
+  const oldPkg = JSON.parse(await git.gitShow(input[0], 'package.json'));
+  const { pkg } = await readPkgUp({ cwd: process.cwd() });
+
+  const delta = diff.diffPackages(oldPkg, pkg);
+  const text = await diff.deltaToMarkdown(delta, oldPkg); // CLI tool, relax!
+  /* eslint-disable no-console */ console.log(text);
+  /* eslint-enable no-console */
 }
 
 module.exports = {
